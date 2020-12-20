c61a98bf18aea7a828654e6de98f2f74d829e1ea
intaeck <intaeck@gmail.com>
2020-12-20 22:13:41 +0900

    husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    // npm i eslint-config-prettier eslint-plugin-prettier
+    // "plugin:prettier/recommended"을 설정해서 prettier와 eslint의 중복설정을 제거해줌
+    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
